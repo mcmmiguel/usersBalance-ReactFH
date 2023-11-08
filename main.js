5c404ba0f11f9b66b5c58af55e01ac601ccbc985
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-07 21:42:31 -0600

    Style. Design colors

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { BreakingBadApp } from './src/brakingbad/breakingbad-app'
 import { UsersApp } from './src/users/users-app'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 id="app-title">Hello Vite!</h1>
-    <div class="card">
+    <h1>USERS BALANCE CONTROL - CRUD</h1>
+    <div class="card container">
     </div>
   </div>
 `
